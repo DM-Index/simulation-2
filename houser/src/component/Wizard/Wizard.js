ddc78781845cf42be4769c887b0ab528c2ef5333
Nathan <n.hatz@yahoo.com>
2018-05-25 12:22:53 -0500

    step 2 finished

--- houser/src/component/Wizard/Wizard.js
+++ houser/src/component/Wizard/Wizard.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import "./Wizard.css";
+
 export default class Wizard extends Component {
   constructor() {
     super();
@@ -12,15 +14,40 @@ export default class Wizard extends Component {
       state: "",
       zipcode: ""
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    console.log(event);
+    this.setState({ value: event.target.value });
   }
   render() {
     return (
       <div>
-        <input className="wizard-input-name" value={this.state.name} />
-        <input className="wizard-input-address" value={this.state.address} />
-        <input className="wizard-input-city" value={this.state.city} />
-        <input className="wizard-input-state" value={this.state.state} />
-        <input className="wizard-input-zipcode" value={this.state.zipcode} />
+        <input
+          className="wizard-input-name"
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <input
+          className="wizard-input-address"
+          onChange={this.handleChange}
+          value={this.state.address}
+        />
+        <input
+          className="wizard-input-city"
+          onChange={this.handleChange}
+          value={this.state.city}
+        />
+        <input
+          className="wizard-input-state"
+          onChange={this.handleChange}
+          value={this.state.state}
+        />
+        <input
+          className="wizard-input-zipcode"
+          onChange={this.handleChange}
+          value={this.state.zipcode}
+        />
         <button className="cancel-btn">
           {" "}
           Cancel
