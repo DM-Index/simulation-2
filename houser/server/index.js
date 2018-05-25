9d15f7f9db1415aa1a6eb22a33447cb0d0eeebaa
Nathan <n.hatz@yahoo.com>
2018-05-25 10:51:30 -0500

    FE, BE, DB Setup

--- houser/server/index.js
+++ houser/server/index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const { json } = require("body-parser");
 const cors = require("cors");
+const massive = require("massive");
 
 const controller = require("./controllers/controller");
 
-const port = 4000;
+const port = process.env.MASSIVE || 4000;
 
 app = express();
 
